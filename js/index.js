ad86c0db09317de18cc2481bbc87d872ea1b3f8d
rainyjune <rainyjune@live.cn>
2014-05-14 16:01:15 +0800

    Add map type control

--- js/index.js
+++ js/index.js
@@ -1,6 +1,24 @@
 var myApp = angular.module("mainApp", []);
 
 myApp.controller("initCtrl", function ($scope) {
-  var position=new AMap.LngLat(116.404,39.915);
-  var mapObj=new AMap.Map("mapContainer",{center:position});
+  var position = new AMap.LngLat(116.404,39.915);
+  var mapOptions = {
+    center: position,
+    level: 12
+  };
+  var mapObj = new AMap.Map("mapContainer", mapOptions);
+  $scope.mapObj = mapObj;
+
+  mapObj.plugin(["AMap.ToolBar"], function(){
+
+    var toolBarOptions = {};
+    if ("geolocation" in navigator) {
+      toolBarOptions.autoPosition = true;
+    }
+    var toolbar = new AMap.ToolBar(toolBarOptions);
+    mapObj.addControl(toolbar);
+    $scope.toolbar = toolbar;
+
+  });
+
 });
