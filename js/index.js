0093384a3cf6054773a10b371538cb6721b2aaad
rainyjune <rainyjune@live.cn>
2014-05-16 17:09:38 +0800

    Using the fluent API

--- js/index.js
+++ js/index.js
@@ -1,25 +1,23 @@
-var myApp = angular.module("mainApp", ["amapServices"]);
+angular.module("mainApp", ["amapServices"])
+  .controller("initCtrl", function ($scope, coreService, geoService, pluginService) {
 
-myApp.controller("initCtrl", function ($scope, coreService, geoService, pluginService) {
-
-  coreService.initMap(function(mapObj){
-    $scope.mapObj = mapObj;
-  });
-
-  pluginService.toolBar($scope.mapObj);
-
-  geoService.getUserCity($scope.mapObj, function(data) {
-    $scope.currentCity = data.city;
-  });
-
-});
-
-myApp.controller("searchCtrl", function ($scope, searchService) {
-  $scope.data = {};
-
-  $scope.submitForm = function(){
-    searchService.placeSearch($scope.data.keyword, $scope.mapObj, $scope.currentCity, function(data) {
-      console.log("Everything is OK:", data);
+    coreService.initMap(function(mapObj){
+      $scope.mapObj = mapObj;
     });
-  };
-});
+
+    pluginService.toolBar($scope.mapObj);
+
+    geoService.getUserCity($scope.mapObj, function(data) {
+      $scope.currentCity = data.city;
+    });
+
+  })
+  .controller("searchCtrl", function ($scope, searchService) {
+    $scope.data = {};
+
+    $scope.submitForm = function(){
+      searchService.placeSearch($scope.data.keyword, $scope.mapObj, $scope.currentCity, function(data) {
+        console.log("Everything is OK:", data);
+      });
+    };
+  });
